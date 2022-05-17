9221d9a22a38898b9ce4a382e6c5a41637528663
stefanvmstreamit <stefanv.mstreamit@outlook.com>
2022-05-17 13:20:03 +0100

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mthree/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Stefan Dragos Vochina
+ *email: stefanv.mstreamit@outlook.com
+ *date: 17/05/2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.mthree.helloworld;
+    
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Elizabeth");
+    }
+}
